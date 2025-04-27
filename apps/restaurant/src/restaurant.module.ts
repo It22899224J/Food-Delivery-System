9aaa016e639d8d2515e55e6f45f3787ef082c3cd
It22899224J <it22899224@my.sliit.lk>
2025-04-27 16:54:54 +0530

    module updated for food item service

--- apps/restaurant/src/restaurant.module.ts
+++ apps/restaurant/src/restaurant.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RestaurantController } from './restaurant.controller';
 import { RestaurantService } from './restaurant.service';
+import { FoodItemController } from './food-item.controller';
+import { FoodItemService } from './food-item.service';
 
 @Module({
   imports: [],
-  controllers: [RestaurantController],
-  providers: [RestaurantService],
+  controllers: [RestaurantController, FoodItemController],
+  providers: [RestaurantService, FoodItemService],
 })
 export class RestaurantModule {}
