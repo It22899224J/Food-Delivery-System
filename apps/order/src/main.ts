fa9d5f431752e6035d58087088cb554a46a9548c
mithilathilochana <mithilathilochanaj@gmail.com>
2025-04-29 22:12:00 +0530

    small bug fix

--- apps/order/src/main.ts
+++ apps/order/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
